dc828a3aa2efa71b7b432d5bdb5e5a7cb7e4954a
wmicheng <1797055971@qq.com>
2022-10-11 13:26:26 +0800

    变量占用字节

--- Project01/05.cpp
+++ Project01/05.cpp
@@ -5,6 +5,9 @@ int main()
 {
     cout << "char: " << sizeof(char) << " 字节" << endl;
     cout << "int: " << sizeof(int) << " 字节" << endl;
+    cout << "long: " << sizeof(long) << " 字节" << endl;
+    cout << "long long: " << sizeof(long long) << " 字节" << endl;
+    cout << "unsigned long long: " << sizeof(unsigned long long) << " 字节" << endl;
     cout << "float: " << sizeof(float) << " 字节" << endl;
     cout << "double: " << sizeof(double) << " 字节" << endl;
 
