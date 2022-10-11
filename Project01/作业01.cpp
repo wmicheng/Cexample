fb0a2cf2f4ffc1e95a037a59b01d6c80a5b7cc46
wmicheng <1797055971@qq.com>
2022-10-11 23:08:56 +0800

    多分支输出成绩的等级

--- Project01/作业01.cpp
+++ Project01/作业01.cpp
@@ -5,19 +5,11 @@ int main() {
 	int score;
 	cin >> score;
 	if (score >= 90)
-	{
 		cout << "成绩优秀！";
-	}
 	else if (score >= 70)
-	{
 		cout << "成绩良好！";
-	}
 	else if (score >= 60)
-	{
 		cout << "成绩及格！";
-	}
 	else if (score < 60)
-	{
 		cout << "不及格！";
-	}
 }
