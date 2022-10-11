8181bab224f2a85435378c1c1fa669010e5cbbc2
wmicheng <1797055971@qq.com>
2022-10-11 12:54:50 +0800

    C++换行

---
+++ Project01/03.cpp
@@ -0,0 +1,20 @@
+//\n »»ÐÐ
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Blog \n";
+    cout << "Google \n";
+    cout << "Taobao";
+    return 0;
+}
+//endl »»ÐÐ
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    cout << "Blog" << endl;
+//    cout << "Google" << endl;
+//    cout << "Taobao";
+//    return 0;
+//}
