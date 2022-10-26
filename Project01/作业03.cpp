e361c3bd27d23f8bf127d414d3cc1bd554a0f35d
wmicheng <1797055971@qq.com>
2022-10-26 19:23:54 +0800

    汉诺塔

--- Project01/作业03.cpp
+++ Project01/作业03.cpp
@@ -1,4 +1,11 @@
 //汉诺塔
+//分析：将n个盘子从A座上移到C座上需要三步：
+//
+//（1）将A上的n - 1个盘子借助C座先移到B座上；
+//
+//（2）把A座上剩下的一个盘子移到C座上；
+//
+//（3）将n - 1个盘从B座上借助于A移到C座上。
 #include <iostream>
 using namespace std;
 void move(char x, char y);
@@ -11,7 +18,8 @@ int main()
 	cout << "移盘的步骤：" << endl;
 	hanoi(n, 'A', 'B', 'C');
 }
-void move(char x, char y)	//输出移盘方案
+void move(char    x, char y)	//输出移盘方案
+
 {
 	cout << x << "->" << y << endl;
 }
